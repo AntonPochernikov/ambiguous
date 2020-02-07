88f911480a106aabb2235ef82886526f09cb99c8
AntonPochernikov <akpochernikov@gmail.com>
2020-02-07 23:04:39 +0300

    - install webpack - add build script

--- index.js
+++
@@ -1,3 +0,0 @@
-export { default as solve } from './src/solve';
-export { amb } from './src/amb';
-export { required } from './src/required';
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as solve } from './solve';
+export { amb } from './amb';
+export { required } from './required';
